dc0a1c19be31e260b8fcba65134a869767b0712a
Kally <kallyasmedia@gmail.com>
2021-08-19 13:12:51 +0000

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8ce21bf27319cc1a194844f685a7f571'),
+    },
+  },
+});
